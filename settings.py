df89fc935d0953caab21ca580ff17fea5f9f4637
Roxanne Devillers <roxannedevillers@gmail.com>
2023-05-05 17:13:54 -0400

    rocket prepared

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    # classe pour stocker les paramètres de jeu"
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
