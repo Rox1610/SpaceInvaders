bf88a61542a078f9da62855a1d1c54502cfd5cca
Roxanne Devillers <roxannedevillers@gmail.com>
2023-05-08 22:05:45 -0400

    creation of start button - ongoing

--- game_stats.py
+++ game_stats.py
@@ -5,5 +5,7 @@ class GameStats:
         self.reset_stats()
         self.game_active = True
 
+        self.game_active = False
+
     def reset_stats(self):
         self.ships_left = self.settings.ship_limit
