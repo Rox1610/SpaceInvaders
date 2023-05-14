1dfd0586429d1083cb2b5d99975f50927715f95a
Roxanne Devillers <roxannedevillers@gmail.com>
2023-05-14 13:48:53 -0400

    button created

--- button.py
+++ button.py
@@ -7,7 +7,7 @@ class Button:
         self.screen_rect = self.screen.get_rect()
 
         self.width, self.height = 200, 50
-        self.button_color = (0, 255, 0)
+        self.button_color = (229, 43, 80)
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
 
@@ -17,6 +17,10 @@ class Button:
         self._prep_msg(msg)
 
     def _prep_msg(self, msg):
-        self.mgs_image = self.font.render(msg, True, self.text_color, self.button_color)
+        self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
