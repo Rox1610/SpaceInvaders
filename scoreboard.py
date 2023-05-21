289a6beba90c0737449ccb571daddbfae36d20bc
Roxanne Devillers <roxannedevillers@gmail.com>
2023-05-21 15:48:06 -0400

    display level

--- scoreboard.py
+++ scoreboard.py
@@ -15,11 +15,12 @@ class Scoreboard:
         self.prep_score()
 
         self.prep_high_score()
+        self.prep_level()
 
     # transform the text to an image
     def prep_score(self):
         rounded_score = round(self.stats.score, -1)
-        score_str = "{:,}".format(rounded_score).replace(",", " ")
+        score_str = "Score: " + "{:,}".format(rounded_score).replace(",", " ")
         self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
 
         # display the score top right of the screen
@@ -27,22 +28,35 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
-    # draw the score on the screen
+    # draw the score and the level on the screen
     def show_score(self):
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
 
     # turn the high score into an image
     def prep_high_score(self):
         high_score = round(self.stats.high_score, -1)
-        high_score_str = "{:,}".format(high_score).replace(",", " ")
-        self.high_score_image = self.font.render(high_score_str, True,
-                self.text_color, self.settings.bg_color)
+        high_score_str = "High score: " + "{:,}".format(high_score).replace(",", " ")
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
 
         # center the high score at the top of the screen
         self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
+
+    # turn the level into an image
+    def prep_level(self):
+        level_str = "Lvl: " + str(self.stats.level)
+        self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
+
+        # Position the level
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.left = self.level_rect.left + 20
+        self.level_rect.top = 20
+        # to display level below score:
+        # self.level_rect.right = self.score_rect.right
+        # self.level_rect.top = self.score_rect.bottom + 10
 
     # check to see if there's a new high score
     def check_high_score(self):
